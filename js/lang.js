63fa0a2a614b3b7769f0b7ebb950b6c050ca0a00
tuananhnguyenhr <94049219+tuananhnguyenhr@users.noreply.github.com>
2025-11-18 11:05:22 +0700

    Create lang.js

---
+++ js/lang.js
@@ -0,0 +1,19 @@
+const langData = {
+    en: {
+        helloText: "Hello!",
+        introText: "I'm Stephen Jhon Scott",
+        descriptionText: "A professional UI/UX & full-stack web developer...",
+        aboutName: "Name: Nguyen Tuan Anh (Alex Nguyen)",
+        aboutAge: "Age: 3x",
+        aboutRole: "Position: Full Stack Web Developer"
+    },
+
+    vi: {
+        helloText: "Xin chào!",
+        introText: "Tôi là Stephen Jhon Scott",
+        descriptionText: "Một chuyên gia UI/UX & lập trình full-stack...",
+        aboutName: "Tên: Nguyễn Tuấn Anh (Alex Nguyễn)",
+        aboutAge: "Tuổi: 3x",
+        aboutRole: "Vị trí: Full Stack Web Developer"
+    }
+};
